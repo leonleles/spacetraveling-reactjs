0d925134de7b9b205151b51c43b83f21b0b5f73d
leonleles <leoleles9159@hotmail.com>
2021-06-21 21:36:45 -0300

    Conclui list posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import { ReactElement } from 'react';
+
+import styles from './header.module.scss';
+import stylesCommon from '../../styles/common.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <div className={styles.headerWrapper}>
+      <div className={stylesCommon.container}>
+        <img src="logo.png" alt="logo" />
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import { ReactElement } from 'react';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import commonStyles from '../styles/common.module.scss';
 
 interface Post {
   uid?: string;
@@ -24,9 +28,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): ReactElement {
+  return (
+    <>
+      <Header />
+      <div className={commonStyles.container}>
+        <ul className={styles.postsWrapper}>
+          <li className={styles.post}>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.postDetails}>
+              <div className={styles.field}>
+                <FiCalendar color="#bbbbbb" size={20} />
+                <span>15 Mar 2021</span>
+              </div>
+              <div className={styles.field}>
+                <FiUser color="#bbbbbb" size={20} />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <button type="button" className={styles.button}>
+          Carregar mais posts
+        </button>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
